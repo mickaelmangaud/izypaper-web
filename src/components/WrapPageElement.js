b88d5dbced1e174703a59b24ebdf3105131fc48a
Mickael Mangaud <mickaelmangaud@gmail.com>
2020-06-30 17:18:49 +0200

    React Helmet

--- src/components/WrapPageElement.js
+++ src/components/WrapPageElement.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { GlobalStyle } from '../styled';
 import { Navigation } from '../components/navigation';
+import { Helmet } from 'react-helmet'
 
 export const WrapPageElement = ({ element }) => {
   return (
     <React.Fragment>
       <GlobalStyle /> 
+      <Helmet>
+        <title>IzyPaper</title>
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+      </Helmet>
       {/* <Burger /> */}
       <Navigation />
       {element}
