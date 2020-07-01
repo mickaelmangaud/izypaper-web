36c6190375f4d371c12818a122458d09486c769a
Mickael Mangaud <mickaelmangaud@gmail.com>
2020-07-01 23:00:28 +0200

    remove window

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,7 @@
 import React from 'react';
 import { IndexPageWrapper } from '../styled';
 
-
-
 export const Index = () => {
-  if(typeof window !== undefined) {
-    console.log('navigateur')
-  } else {
-    console.log('serveur')
-  }
-
   return (
     <IndexPageWrapper>
       <h1>IzyPaper</h1>
