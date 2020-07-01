e6a252c2935da7f019c2c494dc7e28ec302a3e2f
Mickael Mangaud <mickaelmangaud@gmail.com>
2020-07-02 00:37:18 +0200

    login url test

--- src/pages/login.js
+++ src/pages/login.js
@@ -15,7 +15,7 @@ const Login = () => {
   const submitLogin = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('http://localhost:5000/auth/login', formFields,{
+      const response = await axios.post('https://izypaper-backend.herokuapp.com/auth/login', formFields,{
         withCredentials: true,
       });
       console.log(response.status)
