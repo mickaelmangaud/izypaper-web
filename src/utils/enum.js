2890070ffd860311b363d3fa07aed32ce13b1f1f
Mickael Mangaud <mickaelmangaud@gmail.com>
2020-07-08 14:06:50 +0200

    menu items order

--- src/utils/enum.js
+++ src/utils/enum.js
@@ -1,18 +1,18 @@
 export const navLinks = [
-  {
-      label: 'Accueil',
-      path: '/',
-  },
-  {
-      label: 'A propos',
-      path: '/about',
-  },
-  {
-      label: 'Services',
-      path: '/services',
-  },
-  {
-      label: 'Contact',
-      path: '/contact',
-  }
+    {
+        label: 'Accueil',
+        path: '/',
+    },
+    {
+        label: 'Services',
+        path: '/services',
+    },
+    {
+        label: 'A propos',
+        path: '/about',
+    },
+    {
+        label: 'Contact',
+        xxpath: '/contact',
+    }
 ];
