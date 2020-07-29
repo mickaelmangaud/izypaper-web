049f1f2f713fc2606f24376fdf1077d04594b146
Mickael Mangaud <mickaelmangaud@gmail.com>
2020-07-29 12:05:36 +0200

    ssr

---
+++ gatsby-ssr.js
@@ -0,0 +1,5 @@
+import { WrapPageElement } from './src/components/WrapPageElement';
+import { WrapRootElement } from './src/components/WrapRootElement';
+
+export const wrapPageElement = WrapPageElement
+export const wrapRootElement = WrapRootElement
